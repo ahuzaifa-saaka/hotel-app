1694660cf53400d4a1f627f7f66307d7cb2ada52
Abu Saaka Huzaifa <ahuzaifa@persol.com>
2026-07-01 13:28:02 +0000

    pagination

--- src/features/bookings/BookingTable.jsx
+++ src/features/bookings/BookingTable.jsx
@@ -4,6 +4,7 @@ import Table from "../../ui/Table";
 import Menus from "../../ui/Menus";
 import Empty from "../../ui/Empty";
 import Spinner from "../../ui/Spinner";
+import Pagination from "../../ui/Pagination";
 
 function BookingTable() {
   const {bookings, isLoading} = useBookings();
@@ -29,6 +30,10 @@ function BookingTable() {
             <BookingRow key={booking.id} booking={booking} />
           )}
         />
+
+        <Table.Footer>
+          <Pagination count={45} />
+        </Table.Footer>
       </Table>
     </Menus>
   );
--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -1,30 +1,21 @@
 import {getToday} from "../utils/helpers";
 import supabase from "./supabase";
 
-// export async function getBookings() {
-//   const {data, error} = await supabase
-//     .from("bookings")
-//     .select(
-//       "id, created_at, startDate, endDate, numNights, numGuests, status, totalPrice, cabins(name), guests(fullName, email)",
-//     );
-
-//   if (error) {
-//     console.error(error);
-//     throw new Error("Error loading Data");
-//   }
-
-//   return data;
-// }
-
-export async function getBookings() {
-  const {data, error} = await supabase
+export async function getBookings({filter, sortBy}) {
+  let query = supabase
     .from("bookings")
     .select(
       "id, created_at, startDate, endDate, numNight, numGuest, Status, totalPrice, cabins(name), guests(fullName, email)",
     );
 
-  console.log({data});
-  console.log({error});
+  console.log({query});
+
+  if (filter) query = query[filter.method || "eq"](filter.field, filter.value);
+
+  if (sortBy)
+    query = query.order(sortBy.field, {ascending: sortBy.direction === "asc"});
+
+  const {data, error} = await query;
 
   if (error) {
     console.error(error);
